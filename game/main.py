8fa85310a521ba9ba6ed75f6e62f9382c77cdc8b
Zdravko <zdravko.galchov@gmail.com>
2025-02-18 02:20:49 +0000

    Optimized the main game loop, refactored player tank class by adding methods for main functions, so it is more abstract now

--- game/main.py
+++ game/main.py
@@ -1,50 +1,38 @@
 import pygame
-import random
+import sys
+
 
 from player_tank import PlayerTank
 
 
 pygame.init()
 
-SCREEN_SIZE = WIDTH, HEIGHT = 1024, 786
-FPS = 60
-YELLOW = (255,255, 0)
-
-# Set screen parameters
-screen = pygame.display.set_mode(SCREEN_SIZE)
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Battle City")
-
-all_tanks_list = pygame.sprite.Group()
-
-# Initialize player's tank object
-player_tank = PlayerTank(YELLOW, 50, 50)
-# Set its position on the map
-player_tank.rect.x = 10
-player_tank.rect.y = 10
-
 clock = pygame.time.Clock()
-running = True
 
+tank = PlayerTank(WIDTH // 2, HEIGHT // 2, 5)
+
+running = True
 while running:
 
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+
+    keys = pygame.key.get_pressed()
+    tank.move(keys)
+    tank.rotate()
+
     screen.fill("dark grey")
 
-    ### Game functionality ###
+    tank.draw(screen)
 
-    all_tanks_list.add(player_tank)
-    # fill the screen with a color to wipe away anything from last frame
-    all_tanks_list.draw(screen)
-
-
-    # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(FPS)
+    clock.tick(60)
 
 
 pygame.quit()
+sys.exit()
--- game/player_tank.py
+++ game/player_tank.py
@@ -1,20 +1,46 @@
 import pygame
 
 
-class PlayerTank(pygame.sprite.Sprite):
+class PlayerTank:
 
-    def __init__(self, color: str, width: int, height: int) -> None:
-        super().__init__()
-
-        # Create an image of the object
-        self.image = pygame.Surface([width, height])
-
-        # Fill it with a color
-        self.image.fill(color)
-
-        # Draw the rectangle object
-        pygame.draw.rect(self.image, 
-                         color, 
-                         pygame.Rect(0, 0, width, height))
-
+    def __init__(self, x: int, y: int, speed: int) -> None:
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.direction = 'UP'
+        self.original_image = pygame.image.load('/home/zdravko/Programming_Workspace/battle_city/assets/images/pt.png')
+        self.image = pygame.transform.scale(self.original_image, (50, 50))
         self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def move(self, keys):
+        if keys[pygame.K_UP]:
+            self.y -= self.speed
+            self.direction = 'UP'
+        elif keys[pygame.K_DOWN]:
+            self.y += self.speed
+            self.direction = 'DOWN'
+        elif keys[pygame.K_LEFT]:
+            self.x -= self.speed
+            self.direction = 'LEFT'
+        elif keys[pygame.K_RIGHT]:
+            self.x += self.speed
+            self.direction = 'RIGHT'
+
+        self.rect.center = (self.x, self.y)
+
+    def rotate(self):
+        if self.direction == 'UP':
+            self.image = pygame.transform.rotate(self.original_image, 0)
+        elif self.direction == 'DOWN':
+            self.image = pygame.transform.rotate(self.original_image, 180)
+        elif self.direction == 'LEFT':
+            self.image = pygame.transform.rotate(self.original_image, 90)
+        elif self.direction == 'RIGHT':
+            self.image = pygame.transform.rotate(self.original_image, 270)
+
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect(center=self.rect.center)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
