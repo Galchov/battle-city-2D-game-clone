3993d7b7afc9966396745303dbd7b1b383df1d6b
Zdravko <zdravko.galchov@gmail.com>
2025-03-17 19:57:16 +0000

    Added matrix to the screen for the level editor

--- game/level_editor.py
+++ game/level_editor.py
@@ -14,6 +14,19 @@ class LevelEditor:
         self.all_levels = []
 
         self.overlay_screen = self.draw_screen()
+        self.matrix = self.generate_level_matrix()
+
+        self.tile_type = {
+            432: self.assets.brick_tiles["small"],
+            482: self.assets.steel_tiles["small"],
+            483: self.assets.forest_tiles["small"],
+            484: self.assets.ice_tiles["small"],
+            533: self.assets.water_tiles["small_1"],
+            999: self.assets.flag["Phoenix_Alive"]
+        }
+        
+        self.icon_image = self.assets.tank_images["Tank_4"]["Gold"]["Up"][0]
+        self.icon_rect = self.icon_image.get_rect(topleft=(gs.SCREEN_BORDER_LEFT, gs.SCREEN_BORDER_TOP))
 
     def input(self):
         for event in pygame.event.get():
@@ -28,6 +41,10 @@ class LevelEditor:
 
     def draw(self, window):
         window.blit(self.overlay_screen, (0, 0))
+        self.draw_grid_to_screen(window)
+
+        window.blit(self.icon_image, self.icon_rect)
+        pygame.draw.rect(window, gs.GREEN, self.icon_rect, 1)
 
     def draw_screen(self):
         """Generate the game screen"""
@@ -37,3 +54,28 @@ class LevelEditor:
         pygame.draw.rect(overlay_screen, gs.BLACK, (gs.GAME_SCREEN))
 
         return overlay_screen
+    
+    def draw_grid_to_screen(self, window):
+        vertical_lines = (gs.SCREEN_BORDER_RIGHT - gs.SCREEN_BORDER_LEFT) // (gs.IMAGE_SIZE)
+        horizontal_lines = (gs.SCREEN_BORDER_BOTTOM - gs.SCREEN_BORDER_TOP) // (gs.IMAGE_SIZE)
+
+        for i in range(vertical_lines):
+            pygame.draw.line(window, gs.RED, (gs.SCREEN_BORDER_LEFT + (i * gs.IMAGE_SIZE), gs.SCREEN_BORDER_TOP), 
+                                             (gs.SCREEN_BORDER_LEFT + (i * gs.IMAGE_SIZE), gs.SCREEN_BORDER_BOTTOM))
+        
+        for i in range(horizontal_lines):
+            pygame.draw.line(window, gs.RED, (gs.SCREEN_BORDER_LEFT, gs.SCREEN_BORDER_TOP + (i * gs.IMAGE_SIZE)), 
+                                             (gs.SCREEN_BORDER_RIGHT, gs.SCREEN_BORDER_TOP + (i * gs.IMAGE_SIZE)))
+            
+    def generate_level_matrix(self):
+        rows = (gs.SCREEN_BORDER_BOTTOM - gs.SCREEN_BORDER_TOP) // (gs.IMAGE_SIZE // 2)
+        cols = (gs.SCREEN_BORDER_RIGHT - gs.SCREEN_BORDER_LEFT) // (gs.IMAGE_SIZE // 2)
+        matrix = []
+
+        for row in range(rows):
+            line = []
+            for col in range(cols):
+                line.append(-1)
+            matrix.append(line)
+        
+        return matrix
